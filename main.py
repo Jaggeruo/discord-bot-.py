0c753f687aac6f6ebd4689ac91b31d791cada03d
Jaggeruo <55923485+Jaggeruo@users.noreply.github.com>
2021-02-13 16:55:19 +0100

    updated bot attributes, added logout command and added requirements file

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ with open("data.json", "r") as data_file:
     data = json.load(data_file)
     prefix = data["prefix"]
     token = data["token"]
+    owner = data["owner"]
 
 
 client = commands.Bot(
-    command_prefix=prefix, status="online", activity=discord.Game(name="h.help")
+    command_prefix=prefix,
+    status="online",
+    activity=discord.Game(name="h.help"),
+    case_insensitive=False,
+    self_bot=False,
+    help_command=None,
 )
 
 
@@ -18,10 +24,19 @@ async def on_ready():
     print("Bot is ready!")
 
 
-# * ping command - shows bot ping
 @client.command()
 async def ping(ctx):
+    # * Shows bot ping
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+
+@client.command(aliases=["close", "quit"])
+async def logout(ctx):
+    # * End bot connection (only by owner specified in data.json file)
+    if ctx.message.author.id == owner:
+        await client.close()
+    else:
+        await ctx.send("You don't own this bot")
 
 
 client.run(token)
